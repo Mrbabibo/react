a17f011a2428ba137450d4bde15e81f5b51a1bf3
Mrbabibo <Mrbabibo@163.com>
2019-11-29 16:48:04 +0800

    Add files via upload

---
+++ precache-manifest.9939ba6b3841dea2cea6bf77ad0ff373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb56e05ca68bd140c21a68f6cd4ecf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b3b6a8ef28a61e8c2b9",
+    "url": "/static/css/2.e2bd1e95.chunk.css"
+  },
+  {
+    "revision": "fe23e0da238547adcab2",
+    "url": "/static/css/main.e91f630d.chunk.css"
+  },
+  {
+    "revision": "9b3b6a8ef28a61e8c2b9",
+    "url": "/static/js/2.ecfb80a4.chunk.js"
+  },
+  {
+    "revision": "fe23e0da238547adcab2",
+    "url": "/static/js/main.9e081025.chunk.js"
+  },
+  {
+    "revision": "7ced34daf44cb270bc64",
+    "url": "/static/js/runtime-main.9ad9cfcf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
